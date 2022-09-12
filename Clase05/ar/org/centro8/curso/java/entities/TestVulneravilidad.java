718f2dba8e90b48278b9f90269487698928b41d4
Monica Martinez <84873621+moni-mg@users.noreply.github.com>
2022-09-12 18:03:17 -0300

    Delete Clase05/ar/org/centro8/curso/java directory

--- Clase05/ar/org/centro8/curso/java/entities/TestVulneravilidad.java
+++
@@ -1,8 +0,0 @@
-package ar.org.centro8.curso.java.entities;
-
-public class TestVulneravilidad {
-    public static void main(String[] args) {
-        Cuenta cuenta=new Cuenta(1, "arg4");
-        //cuenta.saldo=99999999;
-    }
-}
